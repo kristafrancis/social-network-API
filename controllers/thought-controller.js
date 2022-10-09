400123b39159254e71f385255d1c1393c428f443
Krista Francis <kristafrancis08@yahoo.com>
2022-10-09 13:28:10 -0400

    commented out my plan for controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,21 @@
+
+
+//get all thoughts
+
+
+//get single thought by id
+
+
+//create new thought
+
+
+//update thought by Id
+
+
+//delete thought by Id
+
+
+//create reaction stored in a single thoughts array field
+
+
+//delete reaction by reactions Id
---
+++ controllers/user-controller.js
@@ -0,0 +1,23 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    // get all users
+
+
+    //get user by Id
+
+
+    // create new user
+
+
+    // update user by Id
+
+
+    // delete user by id
+
+
+    // add friend to friends list
+
+
+    // remove friend from friends list
+}
